048131e25f22f35340cf7072434bf9c16e3b9b13
JUSTCOPIED <143vanshagarwal@gmail.com>
2025-08-26 22:27:11 +0530

    Add LearningPage component for educational content

---
+++ app/learning/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+export default function LearningPage() {
+    return (
+        <div>
+            <h1>Learning Page</h1>
+            <p>Welcome to the learning page!</p>   
+            <p>This page is designed to help you learn github and how it works  .</p>
+        </div>
+    );
+}
